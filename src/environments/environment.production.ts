d50abfd0f9bd43921088f8f43a853b1b6f5a9f21
AntRed <anthonyatras@gmail.com>
2025-05-19 15:23:47 -0400

    Arreglando flujo de trabajo para la infraestructura

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api.artilesphotography.com/api",
+  apiUrl: "http://localhost/api",
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+  base: '/backoffice/',
   server: {
     hmr: {
       overlay: false,
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 });
